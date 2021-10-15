7d91cfbff3f525417423f2a64a6c68e8aaf9cc84
niayz10 <niayz10@mail.ru>
2021-10-15 20:32:20 +0600

    Arbitrary task

--- Arbitrary Homework/main_test.go
+++ Arbitrary Homework/main_test.go
@@ -9,6 +9,7 @@ func TestValidityOfInput(t *testing.T) {
 	input := 1.0
 
 	actual, err := ValidityOfInput(input)
+	
 	if err !=nil {
 		t.Errorf("expected number betwen 1 and 5, but got %v", err)
 	}
